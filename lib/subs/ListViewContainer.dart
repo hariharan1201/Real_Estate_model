9f2918b5b81b5a7db47b5aa351592dc6eea83a5b
HariharanM <hariakash1117@gmail.com>
2023-02-10 17:20:42 +0530

    Under development

--- lib/subs/ListViewContainer.dart
+++ lib/subs/ListViewContainer.dart
@@ -21,60 +21,81 @@ class containerlist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      height: 125,
-      width: 200,
-      decoration: BoxDecoration(color: Colors.grey.shade300,borderRadius: BorderRadius.circular(15),),
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            height: 110,
-            width: 110,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: AssetImage(image),fit: BoxFit.fill)),), //Imgae-Holders
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            height: 110,
-            width: 250,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
-            child: Column(
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.all(5),
+        height: 125,
+        width: 200,
+        decoration: BoxDecoration(color: Colors.grey.shade300,borderRadius: BorderRadius.circular(15),),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              height: 110,
+              width: 110,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(image: AssetImage(image),fit: BoxFit.fill)),), //Imgae-Holders
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              height: 110,
+              width: 225,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+              child: Column(
 
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 0,10, 0),
-                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
-                    Text(price,style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Eox2',fontSize: 15),),
-                    Row(
-                      children: [
-                        Icon(Icons.star_border,color:Colors.yellow[700],),
-                        Text(rating,style: TextStyle(fontFamily: 'Poppins',),)
-                      ],
-                    )
-                  ]),
-                ),//Price and rating
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0, 135, 0),
-                  child: Text(infrastructure,style: TextStyle(fontSize: 10,fontFamily: 'Poppins'),),
-                ),//Infrastructure
-                SizedBox(height: 15,),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5,0, 110, 0),
-                  child: Text(name,style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Poppins',fontSize: 15),),
-                ),//Name of property
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0,150, 0),
-                  child: Text(Location,style: TextStyle(fontFamily: 'Poppins',fontSize: 15),),
-                ),//Location
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0,155, 0),
-                  child: Text(Contact,style: TextStyle(fontFamily: 'Poppins',fontSize: 12),),
-                ),//Contact
-              ],
-            ),
-          )
-        ],),
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
+                        Text(price,style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Eox2',fontSize: 15),),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 15),
+                          child: Row(
+                            children: [
+                              Icon(Icons.star_border,color:Colors.yellow[700],),
+                              Text(rating,style: TextStyle(fontFamily: 'Poppins',),)
+                            ],
+                          ),
+                        )
+                      ]),
+                    ), //Price and rating
+                  Row(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
+                        Text(infrastructure,style: TextStyle(fontFamily: 'Poppins',fontSize: 10),),
+                      ]),
+                    ),
+                  ],),
+                  SizedBox(height: 25,),//Infrastructure
+                  Row(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
+                        Text(name,style: TextStyle(fontFamily: 'Poppins',fontSize: 15,fontWeight: FontWeight.bold),),
+                      ]),
+                    ),
+                  ],),//Recidency_Name
+                  Row(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
+                        Text(Location,style: TextStyle(fontFamily: 'Poppins',fontSize: 12),),
+                      ]),
+                    ),
+                  ],),//Location
+                  Row(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children:[
+                        Text(Contact,style: TextStyle(fontFamily: 'Poppins',fontSize: 13,fontWeight: FontWeight.bold),),
+                      ]),
+                    ),
+                  ],)//Contact
+                ],
+              ),
+            )
+          ],),
+      ),
     );
   }
 }
